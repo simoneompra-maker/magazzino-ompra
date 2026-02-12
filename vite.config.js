79d85f802db64031eb76e199c8ccc86836c17397
simoneompra-maker <simone.ompra@gmail.com>
2026-02-12 09:55:42 +0100

    Aggiunto supporto legacy per Android 6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      // Qui diciamo a Vite di creare una versione speciale per Android 6
+      targets: ['defaults', 'not IE 11', 'android >= 6'],
+    }),
+  ],
   server: {
     host: true,
     port: 3000
@@ -10,6 +17,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser'
+    minify: 'terser',
   }
 })
